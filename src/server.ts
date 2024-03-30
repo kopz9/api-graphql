4802caa353fd43b281fe9ae8787ac97cceea9052
kopz9 <gustavoeduardo438@gmail.com>
2024-03-30 20:50:47 -0300

    setup new api

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import "reflect-metadata";
+import {ApolloServer} from "apollo-server";
+
+async function bootstrap() {
+  const server = new ApolloServer({
+    
+  });
+
+  const { url } = await server.listen();
+
+  console.log(`🚀 Server running at ${url}`);
+}
+
+bootstrap();
