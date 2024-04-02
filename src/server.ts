e65a36c4c5d39be903ca5f7fc0ef07c09faac25a
kopz9 <gustavoeduardo438@gmail.com>
2024-04-01 21:44:12 -0300

    feat: add models

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,17 @@
 import "reflect-metadata";
-import {ApolloServer} from "apollo-server";
+import path from 'node:path'
+import { ApolloServer } from "apollo-server";
+import { buildSchema } from "type-graphql";
+import { AppointmentResolver } from "./resolvers/appointments-resolver";
 
 async function bootstrap() {
+  const schema = await buildSchema({
+    resolvers: [AppointmentResolver],
+    emitSchemaFile: path.resolve(__dirname, "schema.gql"),
+  });
+
   const server = new ApolloServer({
-    
+    schema
   });
 
   const { url } = await server.listen();
